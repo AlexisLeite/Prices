fc552ead1486d9c5b52787b2970d3fa263cfb7ba
Alexis Joel Leite Paracampo <alexisleite@live.com>
2021-11-27 22:39:00 -0300

    Correct some errors

--- pages/api/prices.js
+++ pages/api/prices.js
@@ -10,15 +10,15 @@ function replaceAll(s, search, replace) {
 }
 
 const products = [
-  ...JSON.parse(fs.readFileSync('./pages/api/cdrprices.json')).map((product) => ({
+  ...JSON.parse(fs.readFileSync('./cdrprices.json')).map((product) => ({
     ...product,
     store: 'cdr',
   })),
-  ...JSON.parse(fs.readFileSync('./pages/api/toptecnoprices.json')).map((product) => ({
+  ...JSON.parse(fs.readFileSync('./toptecnoprices.json')).map((product) => ({
     ...product,
     store: 'toptecno',
   })),
-  ...JSON.parse(fs.readFileSync('./pages/api/feboprices.json')).map((product, i) => ({
+  ...JSON.parse(fs.readFileSync('./feboprices.json')).map((product, i) => ({
     ...product,
     store: 'febo',
     code: `FEBO${i}`,
